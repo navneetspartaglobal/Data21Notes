cbac5cb7456d2c8f5ff84e5fc6f94c76bc132664
Navneet <npandey@spartaglobal.com>
2021-04-21 10:54:12 +0100

    updated the movie booking code with age validation code

--- Homework.py
+++ Homework.py
@@ -1,32 +1,24 @@
 name = input("What is your Name?")
-age = int(input("what is your age?"))
-#
-# user_prompt = True;
-#
-# while user_prompt:
-#     age=input("what is your age?")
-#     if age.isdigit():
-#         user_prompt = False
-#user_prompt = True
-while True:
-    if age <= 0:
-        print(" Age cannot be negative")
-        break
 
-    elif 70 > age > 16:
-        print(f"Hi {name} you are {age} years old and you can buy General audience ticket")
-        break
-    elif age <= 16:
-        print(f"Hi {name} you are {age} years old and you can not buy tickets without parental guidance")
-        break
-    else:
-        print(f"Hi {name} you are {age} years old and you can enjoy senior citizen privileges")
-        break
-# while True:
-#     age = int(input("what is your age?"))
-#     if age > 16:
-#        print(f"Hi {name} you are {age} years old and you can buy General audience")
-#     elif age<=16:
-#        print(f"Hi {name} you are {age} years old and you can not buy tickets without parental audience")
-#     else:
-#         print("please provide your answer as a positive digit")
+movie_above_16 = ["fast and furious", "Avengers", "Iron man", "Captain America"]
+movie_for_16_and_below = ["Lion King", " Ice age", " Mowgli"]
+movie_for_senior_citizen = [movie_above_16, movie_for_16_and_below]
+
+user_prompt = True
+while user_prompt:
+    age = input("what is your age?")
+    if age.isdigit():
+        user_prompt = False
+
+
+if int(age) <= 0:
+    print(" Age cannot be negative")
+
+elif 65 > int(age) > 16:
+    print(f"Hi {name} you are {age} years old and you can buy tickets for these movies {movie_above_16}")
+
+elif int(age) <= 16:
+    print(f"Hi {name} you are {age} years old and you can buy tickets for these movies {movie_for_16_and_below} ")
+
+else:
+    print(f"Hi {name} you are {age} years old and you can enjoy all these movies {movie_for_senior_citizen}")
