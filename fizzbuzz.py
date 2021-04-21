6a581780452d3298e9ed85d34f5b015d4741cc4c
Navneet <npandey@spartaglobal.com>
2021-04-21 15:27:45 +0100

    importing the function from different files

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,18 @@
-# Fizzbuzz
-num = input("Enter the number")
-for x in range(int(num)):
+from numbers import num_list
+import random
+# num = input("Enter the number")
+# for x in range(int(num)):
+#     if x % 3 == 0 and x % 5 == 0:
+#         print("FizzBuzz")
+#     elif x%3 == 0:
+#         print("Fizz")
+#     elif x%5 ==0:
+#         print("Buzz")
+#     else:
+#         print(x)
+
+
+for x in num_list:
     if x % 3 == 0 and x % 5 == 0:
         print("FizzBuzz")
     elif x%3 == 0:
@@ -9,5 +21,3 @@ for x in range(int(num)):
         print("Buzz")
     else:
         print(x)
-
-
