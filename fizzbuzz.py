de0328fb0a61cdb3c4a58d919b6e2ec4138c6489
Navneet <npandey@spartaglobal.com>
2021-04-28 15:28:22 +0100

    api_changes

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -16,7 +16,7 @@ for x in num_list:
     if x % 3 == 0 and x % 5 == 0:
         print("FizzBuzz")
     elif x%3 == 0:
-        print("Fizz")
+        print("dhgahgdhadhahdhadhgadjhgadj")
     elif x%5 ==0:
         print("Buzz")
     else:
